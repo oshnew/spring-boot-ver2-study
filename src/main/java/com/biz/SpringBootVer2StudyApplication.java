726a410a6775e13c3107183160d289f5d1033448
eomsh <oshnew@naver.com>
2019-01-31 22:48:55 +0900

    first commit

---
+++ src/main/java/com/biz/SpringBootVer2StudyApplication.java
@@ -0,0 +1,14 @@
+package com.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVer2StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVer2StudyApplication.class, args);
+    }
+
+}
+
